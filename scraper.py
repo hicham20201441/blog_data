0c4aff25480dbd840c64bdf0f25119ab10af1530
hicham20201441 <70323150+hicham20201441@users.noreply.github.com>
2020-08-27 14:28:23 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,47 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
-
-# import scraperwiki
-# import lxml.html
+# -*- coding: utf-8 -*-
+import scraperwiki
+import lxml.html
 #
+url="https://dollarsprout.com"
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape(url)
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+urlss=[e.get("href") for e in root.cssselect("a")]
+urls=[]
+for k in urlss:
+    if url in k and url!=k:
+        urls.append(k)
+        scraperwiki.sqlite.save(unique_keys=["link"], data={"link":k})
+nlist=set()
+nlist.add(url)
+while(len(urls)>0):
+    if urls[0] not in nlist:
+        print("scraping: "+urls[0])
+        nlist.add(urls[0])
+        html1= scraperwiki.scrape(urls[0])
+        root1 = lxml.html.fromstring(html1)
+        newrls=[e.get("href") for e in root1.cssselect("a")]
+        for u in newrls:
+            if url in u and url!=u:
+                urls.append(u)
+                scraperwiki.sqlite.save(unique_keys=["link"], data={"link": u})
+        urls.pop(0)
+        
+ 
+        
+    
+  
+    
+  #scraperwiki.sqlite.save(unique_keys=[e.get("href")], data={"link": e.get("href")})
+
+#root.cssselect("div[class='blog-col']")
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+#scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
